f886b72b3c5cc9eb3a7d71f59078c9616a717de1
bamsdws123321 <bbamsdws98@gmail.com>
2026-04-22 17:17:43 +0300

    Add files via upload
    برنامج تحويل العملات

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Currency_exchange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Form_comboBox = new System.Windows.Forms.ComboBox();
+            this.To_comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Convert_but = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Result_textBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(89, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Conversion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(23, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Amount:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(97, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Enter the amount";
+            this.toolTip1.SetToolTip(this.textBox1, "Enter the amount you want transfer");
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // Form_comboBox
+            // 
+            this.Form_comboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.Form_comboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.Form_comboBox.BackColor = System.Drawing.Color.White;
+            this.Form_comboBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Form_comboBox.FormattingEnabled = true;
+            this.Form_comboBox.Items.AddRange(new object[] {
+            "YER",
+            "SAR",
+            "USD",
+            "AED"});
+            this.Form_comboBox.Location = new System.Drawing.Point(97, 141);
+            this.Form_comboBox.Name = "Form_comboBox";
+            this.Form_comboBox.Size = new System.Drawing.Size(143, 21);
+            this.Form_comboBox.TabIndex = 3;
+            this.Form_comboBox.TextChanged += new System.EventHandler(this.Form_comboBox_TextChanged);
+            // 
+            // To_comboBox2
+            // 
+            this.To_comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.To_comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.To_comboBox2.BackColor = System.Drawing.Color.White;
+            this.To_comboBox2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.To_comboBox2.FormattingEnabled = true;
+            this.To_comboBox2.Items.AddRange(new object[] {
+            "YER",
+            "SAR",
+            "USD",
+            "AED"});
+            this.To_comboBox2.Location = new System.Drawing.Point(97, 194);
+            this.To_comboBox2.Name = "To_comboBox2";
+            this.To_comboBox2.Size = new System.Drawing.Size(143, 21);
+            this.To_comboBox2.TabIndex = 4;
+            this.To_comboBox2.TextChanged += new System.EventHandler(this.To_comboBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Silver;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(26, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Form:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(26, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "to";
+            // 
+            // Convert_but
+            // 
+            this.Convert_but.BackColor = System.Drawing.Color.Gray;
+            this.Convert_but.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Convert_but.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Convert_but.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convert_but.ForeColor = System.Drawing.Color.Black;
+            this.Convert_but.Location = new System.Drawing.Point(81, 245);
+            this.Convert_but.Name = "Convert_but";
+            this.Convert_but.Size = new System.Drawing.Size(172, 35);
+            this.Convert_but.TabIndex = 7;
+            this.Convert_but.Text = "CONVERT";
+            this.Convert_but.UseVisualStyleBackColor = false;
+            this.Convert_but.Click += new System.EventHandler(this.Convert_but_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(42, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "RESULT:";
+            // 
+            // Result_textBox
+            // 
+            this.Result_textBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result_textBox.Location = new System.Drawing.Point(107, 324);
+            this.Result_textBox.Name = "Result_textBox";
+            this.Result_textBox.ReadOnly = true;
+            this.Result_textBox.Size = new System.Drawing.Size(146, 21);
+            this.Result_textBox.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.Result_textBox, "The value of the amount that was transferred\r\n");
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(45, 123);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(230, 15);
+            this.label6.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(45, 179);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(224, 12);
+            this.label7.TabIndex = 11;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 200;
+            this.toolTip1.AutoPopDelay = 6000;
+            this.toolTip1.InitialDelay = 200;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 40;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(327, 358);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Result_textBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Convert_but);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.To_comboBox2);
+            this.Controls.Add(this.Form_comboBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Currency exchange";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox Form_comboBox;
+        private System.Windows.Forms.ComboBox To_comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Convert_but;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Result_textBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_exchange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_but_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "Enter the amount" && textBox1.Text != " " )
+            {
+                if (Form_comboBox.Text != "")
+                 {
+                    if (To_comboBox2.Text != "")
+                    {
+
+                        if (Form_comboBox.Text == "YER")
+                        {
+
+                            if (To_comboBox2.Text == "SAR")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) / 612).ToString();
+                            }
+                            else if (To_comboBox2.Text == "USD")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) / 612 / 3.8).ToString();
+                            }
+                            else if (To_comboBox2.Text == "AED")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) / 1.02 / 612).ToString();
+
+                            }
+                            else if (To_comboBox2.Text == "YER")
+                            {
+                                Result_textBox.Text = textBox1.Text;
+
+                            }
+
+                            else
+                            {
+                                Result_textBox.Text = "";
+                            }
+                        }
+
+                        else if (Form_comboBox.Text == "SAR")
+                        {
+                            if (To_comboBox2.Text == "YER")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 612).ToString();
+                            }
+                            else if (To_comboBox2.Text == "USD")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) / 3.8).ToString();
+                            }
+                            else if (To_comboBox2.Text == "AED")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) / 0.98).ToString();
+
+                            }
+                            else if (To_comboBox2.Text == "SAR")
+                            {
+                                Result_textBox.Text = textBox1.Text;
+
+                            }
+
+                            else
+                            {
+                                Result_textBox.Text = "";
+                            }
+                        }
+
+                        else if (Form_comboBox.Text == "USD")
+                        {
+                            if (To_comboBox2.Text == "YER")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 612 * 3.8).ToString();
+                            }
+                            else if (To_comboBox2.Text == "SAR")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 3.8).ToString();
+                            }
+                            else if (To_comboBox2.Text == "AED")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 3.67).ToString();
+
+                            }
+                            else if (To_comboBox2.Text == "USD")
+                            {
+                                Result_textBox.Text = textBox1.Text;
+
+                            }
+
+                            else
+                            {
+                                Result_textBox.Text = "";
+                            }
+                        }
+
+                        else if (Form_comboBox.Text == "AED")
+                        {
+                            if (To_comboBox2.Text == "YER")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 1.02 * 612).ToString();
+                            }
+                            else if (To_comboBox2.Text == "SAR")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 1.02).ToString();
+                            }
+                            else if (To_comboBox2.Text == "USD")
+                            {
+                                Result_textBox.Text = (double.Parse(textBox1.Text.ToString()) * 0.27).ToString();
+
+                            }
+                            else if (To_comboBox2.Text == "AED")
+                            {
+                                Result_textBox.Text = textBox1.Text;
+
+                            }
+
+                            else
+                            {
+                                Result_textBox.Text = "";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter the currency you want to convert to in the Text Box (( To ))");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter the currency you want to convert from in the Text Box (( From: ))");
+                }
+               
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter the amount you want to conver");
+           }
+            }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Enter the amount")
+            {
+                
+                    textBox1.Text = "";
+                    
+                    
+
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Enter the amount";
+
+                textBox1.ForeColor = Color.Silver;
+            }
+        }
+
+        private void Number_only(KeyPressEventArgs e)
+        {
+          switch(e.KeyChar)
+            {
+                case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case (char)Keys.Back:case (char)Keys.Enter:
+                    e.Handled = false;
+                    break;
+
+                case '.':
+                    if(!textBox1.Text.Contains('.'))
+                    {
+                        e.Handled = false;
+                    }
+                    else
+                    {
+                        e.Handled = true;
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Please use number only , your use is : " + e.KeyChar);
+                    e.Handled = true;
+                    break;
+
+            }
+        
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Number_only(e);       
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter )
+            {
+                Convert_but.PerformClick();
+            }
+           
+        }
+
+        private void Form_comboBox_TextChanged(object sender, EventArgs e)
+        {
+            if (Form_comboBox.Text == "AED")
+                label6.Text = "Transfer the amount to Emirati Dirham";
+                
+            else if (Form_comboBox.Text == "YER")
+                label6.Text = "Transfer the amount from Yemeni Riyals";
+
+            else if (Form_comboBox.Text == "SAR")
+                label6.Text = "Transfer the amount from Saudi Riyals";
+
+            else if (Form_comboBox.Text == "USD")
+                label6.Text = "Transfer the amount from American Dollar";
+
+            else
+                label6.Text = "";
+            
+
+        }
+
+        private void To_comboBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (To_comboBox2.Text == "AED")                      
+                label7.Text = "Transfer the amount to Emirati Dirham "; 
+                      
+            else if (To_comboBox2.Text == "YER")
+                label7.Text = "Transfer the amount to Yemeni Riyals";
+
+            else if (To_comboBox2.Text == "SAR")
+                label7.Text = "Transfer the amount to Saudi Riyals";
+
+            else if (To_comboBox2.Text == "USD")
+                label7.Text = "Transfer the amount to American Dollar";
+
+            else
+                label7.Text = " ";
+        }
+    }
+}
